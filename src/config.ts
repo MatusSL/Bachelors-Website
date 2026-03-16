34bb9b2f23fa470a3e10d07b8dbace4aebf97b25
MatusSL <matusslechta@gmail.com>
2026-03-16 18:25:23 +0100

    Updatd weekly report 2

--- src/config.ts
+++ src/config.ts
@@ -2,69 +2,68 @@ export const weeeklyReports = [
   {
     id: 1,
     text: "Návrh flowchart diagramu znázorňujúci priebeh konverzácie chatbota s používateľom",
-    date: "(2.3. - 8.3.)"
+    date: "(2.3. - 8.3.)",
   },
   {
     id: 2,
-    text: "",
-    date: "(9.3. - 15.3.)"
+    text: "Tento týždeň som vytvoril JSON schémy pre všetky štyri typy aplikácií — webová, mobilná, desktopová a API. Schémy slúžia ako základ pre generovanie testovacieho checklistu v chatbote.",
+    date: "(9.3. - 15.3.)",
   },
   {
     id: 3,
     text: "",
-    date: "(16.3. - 22.3.)"
+    date: "(16.3. - 22.3.)",
   },
   {
     id: 4,
     text: "",
-    date: "(23.3. - 29.3.)"
+    date: "(23.3. - 29.3.)",
   },
   {
     id: 5,
     text: "",
-    date: "(30.3. - 5.4.)"
+    date: "(30.3. - 5.4.)",
   },
   {
     id: 6,
     text: "",
-    date: "(6.4. - 12.4.)"
+    date: "(6.4. - 12.4.)",
   },
   {
     id: 7,
     text: "",
-    date: "(13.4. - 19.4.)"
+    date: "(13.4. - 19.4.)",
   },
   {
     id: 8,
     text: "",
-    date: "(20.4. - 26.4.)"
+    date: "(20.4. - 26.4.)",
   },
   {
     id: 9,
     text: "",
-    date: "(27.4. - 3.5.)"
+    date: "(27.4. - 3.5.)",
   },
   {
     id: 10,
     text: "",
-    date: "(4.5. - 10.5.)"
+    date: "(4.5. - 10.5.)",
   },
   {
     id: 11,
     text: "",
-    date: "(11.5. - 17.5.)"
+    date: "(11.5. - 17.5.)",
   },
   {
     id: 12,
     text: "",
-    date: "(18.5. - 24.5.)"
+    date: "(18.5. - 24.5.)",
   },
   {
     id: 13,
     text: "",
-    date: "(25.5.)"
+    date: "(25.5.)",
   },
 ];
 
-
-export const sources = []
+export const sources = [];
